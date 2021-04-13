1dc7aa2080270270a578d5aed9f9fe23a70cd8fc
1303279512@qq.com <1303279512@qq.com>
2021-04-13 16:48:52 +0800

    newVersion

---
+++ src/main/java/com/mia/tjacoco/NewOne.java
@@ -0,0 +1,16 @@
+package com.mia.tjacoco;
+
+public class NewOne {
+
+
+    public static int getResutl(int m,int n){
+        if(m>0){
+            return m;
+        }
+        if(n>0){
+            return n;
+        }
+        return m+n;
+    }
+
+}
